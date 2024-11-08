160a17098f3dfc64f32951ec286f283d008577eb
mondalpritam <105710526+mondalpritam@users.noreply.github.com>
2024-11-08 11:00:28 +0530

    Update PRITAMVPSSCRIPT.sh

--- PRITAMVPSSCRIPT.sh
+++ PRITAMVPSSCRIPT.sh
@@ -343,18 +343,18 @@ apt autoremove -y
 rm -fr /etc/issue.net
 cat> /etc/issue.net << END
 </strong> <p style="text-align:center"><b> <br><font 
-color="#00FFE2"<br>┏━━━━━━━━━━━━━━━┓<br>𝐔𝐍𝐈𝐕𝐄𝐑𝐒𝐄
+color="#00FFE2"<br>┏━━━━━━━━━━━━━━━┓<br>𝐀𝐂𝐄
 𝐍𝐄𝐓𝐖𝐎𝐑𝐊<br>┗━━━━━━━━━━━━━━━┛<br></font><br><font 
 color="#00FF00"></strong> <p style="text-align:center"><b> <br><font 
 color="#00FFE2">क═══════क⊹⊱✫⊰⊹क═══════क</font><br><font 
-color='#FFFF00'><b> ★ [ 𝐔𝐍𝐈𝐕𝐄𝐑𝐒𝐄 𝐍𝐄𝐓𝐖𝐎𝐑𝐊 ] ★ 
-</b></font><br><font color="#FFF00">PAID SCRIPT</font><br> <font color="#FF00FF">❖Ƭʜᴇ No DDOS</font><br> 
+color='#FFFF00'><b> ★ [ 𝐀𝐂𝐄 𝐍𝐄𝐓𝐖𝐎𝐑𝐊 ] ★ 
+</b></font><br><font color="#FFF00">SCRIPT BY PRITAM</font><br> <font color="#FF00FF">❖ @professorLORD</font><br> 
 <font color="#FF0000">❖Ƭʜᴇ No XXX/font><br> <font 
 color="#FFB1C2">❖Ƭʜᴇ No BP </font><br> <font color="#FFFFFF">❖Ƭʜᴇ No 
 Hacking</font><br> <font color="#00FF00">❖Ƭʜᴇ No PORN</font><br> <font 
-color="#00FF00">▄︻デ𝐔𝐍𝐈𝐕𝐄𝐑𝐒𝐄 𝐍𝐄𝐓𝐖𝐎𝐑𝐊══━一 </font><br> <font 
+color="#00FF00">▄︻デ𝐀𝐂𝐄 𝐍𝐄𝐓𝐖𝐎𝐑𝐊══━一 </font><br> <font 
 color="#00FFE2">क═══════क⊹⊱✫⊰⊹क═══════क</font><br></font><br><font 
-color="FFFF00">@universe9720</font><br>
+color="FFFF00">@professorLORD [PRITAM] </font><br>
 END
 cd
 chown -R www-data:www-data /home/vps/public_html
